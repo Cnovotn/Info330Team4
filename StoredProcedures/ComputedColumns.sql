fa7cab613152ea9aa940975b56cec0062a4d1042
Jeffrey <Jeffrey@Jeffreys-MacBook-Pro.local>
2019-12-06 01:11:31 -0800

    player 1 team business rule

--- StoredProcedures/ComputedColumns.sql
+++ StoredProcedures/ComputedColumns.sql
@@ -26,3 +26,26 @@ ALTER TABLE GAMES
 ADD Total2PointersMissed AS (dbo.ComputeTotalStatType(GameID, '2Missed'))
 GO
 
+CREATE FUNCTION PlayerOnlyOneTeam
+RETURNS INT
+AS
+BEGIN
+DECLARE @Ret INT = 0
+IF EXISTS (SELECT P.PlayerID
+           FROM PLAYERS P
+           JOIN TEAM_ORDER TO on TO.PlayerID = P.PlayerID
+           JOIN TEAMS T on T.TeamID = TO.TeamID
+           JOIN (SELECT TO2.PlayerID, TO2.TeamID
+                 FROM TEAM_ORDER TO2) AS SUBQ
+           WHERE TO.PlayerID = SUBQ.PlayerID
+           AND TO.TeamID != SUBQ.TeamID)
+    BEGIN
+        SET @Ret = 1
+    END
+RETURN @Ret
+END
+GO
+
+ALTER TABLE TEAM_ORDER
+ADD CONSTRAINT CK_PlayerOnlyOneTeam
+AS (dbo.PlayerOnlyOneTeam() = 0)
