e8e0bb7cbad6082c274898b9b35201493e5bb1cf
Clayton Novotney <claytonnovotney@gmail.com>
2019-12-06 17:29:33 -0800

    addded some fixing code

--- StoredProcedures/ComputedColumns.sql
+++ StoredProcedures/ComputedColumns.sql
@@ -1,5 +1,3 @@
--- Compute Single player stats
-
 CREATE FUNCTION ComputeTotalStatType(@GameID INT, @StatType VARCHAR(50))
 RETURNS INT
 AS
@@ -26,3 +24,17 @@ ALTER TABLE GAMES
 ADD Total2PointersMissed AS (dbo.ComputeTotalStatType(GameID, '2Missed'))
 GO
 
+CREATE FUNCTION ComputeTotalHomeFouls(@GameID INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @RET INT
+    SET @RET = (
+        SELECT Count(PGS.PlayerGameStatID) 
+        FROM PLAYER_GAME_STATS PGS
+        JOIN PLAYER_GAME_STATS_TYPE PGST ON PGS.PlayerGameStatTypeID = PGST.PlayerGameStatTypeID
+        WHERE PGST.StatName = 'Foul' AND PGS.GameID = @GameID 
+    )
+    RETURN @RET
+END 
+GO
